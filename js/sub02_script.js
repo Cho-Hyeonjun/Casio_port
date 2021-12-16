a943b6efe6cc2c3cba8c17ec9d3c332e1121b7a0
Cho-Hyeonjun <95673836+Cho-Hyeonjun@users.noreply.github.com>
2021-12-16 17:31:07 +0900

    1216script

--- js/sub02_script.js
+++ js/sub02_script.js
@@ -57,26 +57,26 @@ $(document).ready(function(){ //START
 
   function mobileBox() {
     console.log("m");
-    // $(".helfBtn ul").show();
+    $(".helfBtn ul").show();
 
-    $(".helfBtn").mouseover(function(){
-      $(this).children("ul").stop().show();
-      $(this).removeClass("on")
-    });
-    $(".helfBtn").mouseout(function(){
-      $(".helfBtn").children("ul").stop().show();
-      $(".helfBtn").removeClass("on")
-    });
-    $(".sub02_visual .helf li").css({"margin-bottom":"10px"}) 
+    // $(".helfBtn").mouseover(function(){
+    //   $(this).children("ul").stop().show();
+    //   $(this).removeClass("on")
+    // });
+    // $(".helfBtn").mouseout(function(){
+    //   $(".helfBtn").children("ul").stop().show();
+    //   $(".helfBtn").removeClass("on")
+    // });
+    // $(".sub02_visual .helf li").css({"margin-bottom":"10px"}) 
   }
 
   function pcBox() {
     console.log("pc");
     // PC에서 실행 내용
-    // $(".helfBtn ul").hide();
+    $(".helfBtn ul").hide();
 
     $(".helfBtn").click(function(){
-      $(this).find("ul").slideToggle(function(){
+      $(this).find("ul").stop().slideToggle(function(){
         $(this).parents(".helfBtn").toggleClass("on");
       });
     
